d4d092c868eb130f406b0501129fc69f5ed3fe49
mehdi0333 <164217277+benabdallahSamir@users.noreply.github.com>
2025-08-29 17:59:56 +0100

    handle layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import UserLayout from "./components/layout/UserLayout";
+import AdminLayout from "./components/layout/AdminLayout";
 
 export default function App() {
   return (
-    <div className=''>App</div>
-  )
+    <div className="">
+      <Routes>
+        <Route path="/" element={<UserLayout />}>
+          {/* user layout */}
+        </Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          {/* admin layout */}
+        </Route>
+      </Routes>
+    </div>
+  );
 }
